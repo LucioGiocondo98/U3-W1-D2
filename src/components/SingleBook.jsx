34d7fc9d2e08ae76296bff33516637838dd6f1fe
LucioGiocondo98 <togaluc@hotmail.com>
2025-04-02 18:03:52 +0200

    little fixed

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -33,30 +33,26 @@ class SingleBook extends Component {
     const { book } = this.props;
     const { selected } = this.state;
     return (
-      <Container>
+      <Col>
         <h2>SingleBook</h2>
-        <Row className="g-3">
-          <Col>
-            <Card
-              style={{
-                minHeight: "275px",
-                border: selected ? "5px solid red" : "1px solid gray",
-              }}
-              onClick={() => {
-                this.setState({
-                  selected: !this.state.selected,
-                });
-              }}
-            >
-              <Card.Img variant="top" src={book.img} alt={book.title} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>{book.price + "$"}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+        <Card
+          style={{
+            minHeight: "275px",
+            border: selected ? "5px solid red" : "1px solid gray",
+          }}
+          onClick={() => {
+            this.setState({
+              selected: !this.state.selected,
+            });
+          }}
+        >
+          <Card.Img variant="top" src={book.img} alt={book.title} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>{book.price + "$"}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
     );
   }
 }
