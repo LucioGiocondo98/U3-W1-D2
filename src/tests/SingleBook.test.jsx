26fd586cb71e489eabb1d532c7e3c1d39b0476b9
LucioGiocondo98 <togaluc@hotmail.com>
2025-04-10 18:01:27 +0200

    fix

--- src/tests/SingleBook.test.jsx
+++ src/tests/SingleBook.test.jsx
@@ -1,11 +1,13 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect } from "vitest";
-import SingleBook from "../components/SingleBook";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, expect, vi } from "vitest";
+
+import BookList from "../components/BookList";
 
 describe("testing card", () => {
   it("changes border from grey to red when clicked", () => {
-    render(<SingleBook />);
-    const grayBorderCard = screen.getBYTestId("grayCard");
+    render(<BookList />);
+
+    const grayBorderCard = screen.getByTestId("greyCard");
     expect(grayBorderCard).toHaveStyle("border: 1px solid gray");
     fireEvent.click(grayBorderCard);
     expect(grayBorderCard).toHaveStyle("border:5px solid red");
